21272af297e21e58f11cf0f4738498b92ccb95c8
Paulo Cesar <email@pocesar.e4ward.com>
2020-06-24 13:58:41 -0300

    add interval / buffering pushData

--- examples/storage.js
+++ examples/storage.js
@@ -28,3 +28,51 @@ exports.createPersistedMap = async (name, key) => {
       key,
   };
 };
+
+/**
+ * Intervaled dataset.pushData and provide a way to deduplicate
+ * while pushing, by using a key.
+ *
+ * Saves the pending items to the KV in case of migration
+ *
+ * @param {Apify.Dataset} dataset
+ * @param {number} [limit]
+ */
+exports.intervalPushData = async (dataset, limit = 50000) => {
+    const data = new Map(await Apify.getValue('PENDING_PUSH'));
+    await Apify.setValue('PENDING_PUSH', []);
+
+    Apify.events.on('migrating', async () => {
+        await Apify.setValue('PENDING_PUSH', [...data.entries()]);
+    });
+
+    const interval = setInterval(async () => {
+        if (data.size >= limit) {
+            const dataToPush = [...data.values()];
+            data.clear();
+            await dataset.pushData(dataToPush);
+        }
+    }, 10000);
+
+    return {
+        /**
+         * @param {string} key
+         * @param {any} item
+         * @returns {boolean} Returns true if the item is new
+         */
+        pushData(key, item) {
+            const isNew = !data.has(key);
+            data.set(key, item);
+            return isNew;
+        },
+        async flush() {
+            clearInterval(interval);
+
+            const dataToPush = [...data.values()];
+
+            while (dataToPush.length) {
+                await Apify.pushData(dataToPush.splice(0, limit));
+            }
+        }
+    }
+}
