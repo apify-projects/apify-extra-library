7d2629c3e67fd44538308d0bf9821d3c746ce638
Paulo Cesar <email@pocesar.e4ward.com>
2020-10-01 01:57:30 -0300

    persistedCall

--- copy-paste/state-persistence.js
+++ copy-paste/state-persistence.js
@@ -1,4 +1,5 @@
 const Apify = require('apify');
+const { createHash } = require('crypto');
 
 /**
  * Create and return a Map that is persisted to KV on 'persistState'
@@ -7,7 +8,7 @@ const Apify = require('apify');
  * @param {string} storeName
  * @param {string} recordKey
  */
-exports.createPersistedMap = async (storeName, recordKey) => {
+const createPersistedMap = async (storeName, recordKey) => {
     const kv = await Apify.openKeyValueStore(storeName);
 
     /** @type {Map<string, any>} */
@@ -27,4 +28,70 @@ exports.createPersistedMap = async (storeName, recordKey) => {
         storeName,
         recordKey,
     };
+};
+
+/**
+ * Make Apify.call idempotent.
+ * Wraps the key value store of your choice and keeps the call
+ * states there. Same inputs always yield the same call.
+ *
+ * Provide the idempotencyKey manually to be able to create
+ * more calls using the same input, since it defaults to the
+ * current Run ID
+ *
+ * @param {Apify.KeyValueStore} kv
+ */
+const persistedCall = async (kv) => {
+    /**
+     * @type {Map<string, { runId: string, actorId: string }>}
+     */
+    const calls = new Map(await kv.getValue('CALLS'));
+
+    const persistState = async () => {
+        await kv.setValue('CALLS', [...calls.entries()]);
+    };
+
+    Apify.events.on('persistState', persistState);
+
+    /**
+     * @param {string} actorName
+     * @param {any} [input]
+     * @param {any} [options]
+     */
+    const fn = async (actorName, input = {}, options = {}, idempotencyKey = Apify.getEnv().actorRunId) => {
+        const inputHash = createHash('md5', { autoDestroy: true })
+            .update(`${actorName}${JSON.stringify({ input, options })}${idempotencyKey}`)
+            .digest('hex');
+
+        if (calls.has(inputHash)) {
+            const call = calls.get(inputHash);
+
+            return Apify.utils.waitForRunToFinish({
+                ...options,
+                ...call,
+            });
+        }
+
+        const run = await Apify.call(actorName, input, { ...options, waitSecs: 0 });
+
+        calls.set(inputHash, {
+            runId: run.id,
+            actorId: run.actId,
+        });
+
+        await persistState();
+
+        return Apify.utils.waitForRunToFinish({
+            actorId: run.actId,
+            runId: run.actId,
+            waitSecs: options.waitSecs,
+        });
+    };
+
+    return fn;
+};
+
+module.exports = {
+    persistedCall,
+    createPersistedMap,
 };
