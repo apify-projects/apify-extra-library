1c3c724e093c9fa09389e786ac0c957c32ab6f8b
metalwarrior665 <lukaskrivka@gmail.com>
2022-04-05 10:56:19 +0200

    update 0 concurrency bug

--- src/0concurrency.js
+++ src/0concurrency.js
@@ -2,23 +2,38 @@ const Apify = require('apify');
 
 const { log } = Apify.utils;
 
-const isDoingNothing = async (crawler) => {
-    if (crawler.autoscaledPool?.currentConcurrency === 0) {
-        const info = await crawler.requestQueue?.getInfo?.();
-        if (!info || info.pendingRequestCount <= 0) {
-            return true;
-        }
+const handleResurrect = async (crawler) => {
+    const runId = Apify.getEnv().actorRunId;
+    await Apify.addWebhook({
+        eventTypes: ['SUCCEEDED', 'ABORTED', 'FAILED'],
+        requestUrl: `https://api.apify.com/v2/actor-runs/${runId}/resurrect?token=${Apify.getEnv().token}`,
+    });
+    await crawler.autoscaledPool.abort();
+};
+
+const handleAbort = async (crawler) => {
+    const queueInfo = await crawler.requestQueue?.getInfo?.();
+    const listHasRequests = await crawler.requestList?.isFinished?.() === false;
+    const hasStillRequests = queueInfo?.pendingRequestCount > 0 || listHasRequests;
+
+    if (hasStillRequests) {
+        await handleResurrect(crawler);
+    } else {
+        await crawler.autoscaledPool.abort();
     }
 };
 
-const abortIfStuck = async (crawler, state) => {
-    const MAX_DOING_NOTHING_SECS = 60;
+const isDoingNothing = async (crawler) => {
+    return crawler.autoscaledPool?.currentConcurrency === 0;
+};
+
+const abortIfStuck = async (crawler) => {
+    const MAX_DOING_NOTHING_SECS = 120;
     const POLLING_INTERVAL_SECS = 5;
     let doingNothingInRowSecs = 0;
     for (;;) {
         await Apify.utils.sleep(POLLING_INTERVAL_SECS * 1000);
-        // state gives us info if the Promise.race finished so we can clean up this function
-        if (state.raceFinished || crawler.autoscaledPool?.isStopped) {
+        if (crawler.autoscaledPool?.isStopped) {
             return;
         }
         log.debug('0 Concurrency health check: Checking if the crawler is stuck at 0 concurrency');
@@ -32,16 +47,14 @@ const abortIfStuck = async (crawler, state) => {
         if (doingNothingInRowSecs > MAX_DOING_NOTHING_SECS) {
             log.warning(`0 Concurrency health check: Aborting crawler for being at 0 concurrency for more than ${MAX_DOING_NOTHING_SECS} seconds`);
             // Here we abort the crawler, wait a bit and then return to clean the promise
-            await crawler.autoscaledPool.abort();
+            await handleAbort(crawler);
         }
     }
 };
 
 module.exports.runAndProtect0Concurrency = async (crawler) => {
-    const state = { raceFinished: false };
     await Promise.race([
         crawler.run(),
-        abortIfStuck(crawler, state),
+        abortIfStuck(crawler),
     ]);
-    state.raceFinished = true;
 };
